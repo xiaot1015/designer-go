b967ca6276e41fe84c6fabbac961d062a13963c8
xiaotao <xiaotao@100tal.com>
2022-01-19 19:05:30 +0800

    fix

--- mode-state/app/service/statemachine.go
+++ mode-state/app/service/statemachine.go
@@ -1,1 +1,18 @@
 package service
+
+import "state/app/inter"
+
+type StateMachine struct {
+	Ordered inter.State
+	OrderPaying inter.State
+	OrderPay inter.State
+	OrderCancel inter.State
+	OrderRefund inter.State
+
+	CurrentState inter.State
+}
+
+func NewStateMachine() *StateMachine {
+	obj := new(StateMachine)
+	return obj
+}
